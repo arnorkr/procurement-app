10e55142accd43051968dc1c2284c298326338d7
Arnor <7689495+arnorkr@users.noreply.github.com>
2024-03-25 09:09:13 +0100

    Add PDF upload to front page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,37 +5,46 @@ import ArticleIcon from '@mui/icons-material/Article';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 
 const IndexPage = () => {
-  return (
-    <>
-      <Box sx={{ flexGrow: 1, mt: 15, mb: 15 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-            {/* TODO: extract a FrontPageButton component*/}
-              <Button
-                variant="contained"
-                color="primary"
-                href="/request-submission"
-                className="custom-button"
-                startIcon={<ArticleIcon/>}
-              >
-                New Request
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                className="custom-button"
-                href="/overview"
-                startIcon={<LightbulbIcon/>}
-              >
-              Request Overview
-              </Button>
+    return (
+        <>
+            <Box sx={{ flexGrow: 1, mt: 15, mb: 15 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Box sx={{ display: "flex", justifyContent: "center" }}>
+                            {/* TODO: extract a FrontPageButton component*/}
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                href="/request-submission"
+                                className="custom-button"
+                                startIcon={<ArticleIcon />}
+                            >
+                                New Request
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                href="/upload_document"
+                                className="custom-button"
+                                startIcon={<ArticleIcon />}
+                            >
+                                Upload PDF
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className="custom-button"
+                                href="/overview"
+                                startIcon={<LightbulbIcon />}
+                            >
+                                Request Overview
+                            </Button>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    </>
-  );
+        </>
+    );
 };
 
 export default IndexPage;
